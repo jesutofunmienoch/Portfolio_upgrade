dfd4a5ba528f66b94b6a2b421f5776373cbd6dc1
Enoch Jesutofunmi <enochtofunmi81@gmail.com>
2025-07-06 16:12:45 +0100

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import LogoShowcase from "./sections/LogoShowcase";
 import FeatureCards from "./sections/FeatureCards";
-
+import NavBar from "./components/NavBar";
 
 const App = () => (
   <>
-    
+    <NavBar />
     <Hero />
     <ShowcaseSection />
     <LogoShowcase />
